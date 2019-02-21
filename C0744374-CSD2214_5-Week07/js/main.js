1c5768ea5c5c63898186c6ae850b3a89b4b24260
744374 <744374@230-PC04>
2019-02-21 08:42:47 -0500

    commit 8:42

--- C0744374-CSD2214_5-Week07/js/main.js
+++ C0744374-CSD2214_5-Week07/js/main.js
@@ -1,1 +1,21 @@
-﻿// Your code here!
+﻿var bird={
+    genus:'corvus',
+    species:'corvax',
+    commanName:'raven',
+    callType:'squawky'
+    quote:'Nevermore',
+    maxOffSpring:5,
+    noisy:true,
+    deadly:false
+};
+
+var bear={
+    genus:'ursus',
+    species:'arctos',
+    commanName:'brown bear',
+    callType:'roar'
+    quote:'',
+    maxOffSpring:3,
+    noisy:true,
+    deadly:true
+};
